eea51c55a0d656e2b9230cacc05cf91c73e6e5df
pchambers <peter.chambers1@gmail.com>
2018-11-27 00:09:18 +0000

    whoops

--- sigk/api/api_test1.py
+++ sigk/api/api_test1.py
@@ -2,7 +2,7 @@
 curl http://vanguard-drone.local:3443/signalk/v1/api/
 
 
-curl -k https://vanguard-drone.local:3443/sigk/v1/api/vessels/self/
+curl -k https://vanguard-drone.local:3443/signalk/v1/api/vessels/self/
 # confirmed valid construction for curl
 
 
